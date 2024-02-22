6c13c445b11143d4ee138c7ee0c0d1687d0cb98d
Pavlov Pavel <pavlovpavelv1018@gmail.com>
2024-02-22 19:35:25 +0100

    Add_SeleniumTest

---
+++ src/test/java/com/ait/qa31/FindElementTests.java
@@ -0,0 +1,47 @@
+package com.ait.qa31;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.xml.datatype.Duration;
+import java.util.List;
+
+public class FindElementTests {
+
+    static WebDriver driver;
+    @BeforeMethod
+    public static void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://ilcarro.web.app");
+
+        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @Test
+    public void findElementByTagName(){
+        WebElement element = driver.findElement(By.tagName("h1"));
+        System.out.println(element.getText());
+        WebElement element1 = driver.findElement(By.tagName("a"));
+
+        System.out.println(element1.getText());
+        List<WebElement> elements = driver.findElements(By.tagName("a"));
+        System.out.println(elements.size());
+
+    }
+    @Test
+    public void findElementBySampleLocator(){
+        driver.findElement(By.id("city"));
+        WebElement element = driver.findElement(By.className("navigation-link"));
+        System.out.println(element.getText());
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
